b463c47d18fab0ec55e93ef8f1b20c7d28f75a44
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-01-01 21:49:30 +0500

    finance-main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import csv
+from datetime import datetime
+
+
+class CSV:
+    CSV_FILE = "finance_data.csv"
+    
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=["data", "amount","category","description"])
+
+
